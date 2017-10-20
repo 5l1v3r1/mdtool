0839fe4561a8e8fefd78b173908c3a3b15fafa53
minedevelopes <rivan726@gmail.com>
2017-10-20 10:03:06 +0000

    Add files via upload

---
+++ MDTool/mdtool.py
@@ -0,0 +1,22 @@
+
+# imports
+
+import os
+import sys
+import time
+import random
+
+# main
+
+os.system('clear')
+print(' __  __ ____ _____           _                               ')
+print('|  \/  |  _ \_   _|__   ___ | |    Welcome to MDTool > V5.3  ')
+print('| |\/| | | | || |/ _ \ / _ \| | --> MADE BY Rivan Juthani <--')
+print('| |  | | |_| || | (_) | (_) | | --> http://WEBSITECOMINGSOON/<--')
+print('|_|  |_|____/ |_|\___/ \___/|_| Please read README.md before use')
+print('           ----> MDTool <:> MultiDoableTool <----               ')
+print('[][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][]')
+usrinput = input('[mdtoolusr@MDT-box]$ ')
+if usrinput = 'help':
+	
+
